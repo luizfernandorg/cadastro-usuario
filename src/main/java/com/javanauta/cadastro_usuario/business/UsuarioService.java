08f056448c5b16584e7fae7afc83ebf55f2a3fb4
Luiz Fernando <luizfernandorg@gmail.com>
2025-10-05 10:32:17 -0300

    Added CRUD methods

--- src/main/java/com/javanauta/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/javanauta/cadastro_usuario/business/UsuarioService.java
@@ -1,10 +1,43 @@
 package com.javanauta.cadastro_usuario.business;
 
+import com.javanauta.cadastro_usuario.infrastructure.entitys.Usuario;
+import com.javanauta.cadastro_usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class UsuarioService {
 
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void salvarUsuario(Usuario usuario){
+        usuarioRepository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
+    }
+
+    public void atualizaUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado")
+        );
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+        usuarioRepository.saveAndFlush(usuarioAtualizado);
+    }
 }
